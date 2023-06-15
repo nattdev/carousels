bcb75839015bbd0ee05721505d5dc3017e5fef83
Vanessa <110750471+nattdev@users.noreply.github.com>
2023-06-15 01:40:45 -0500

    Update infinite carousel

--- carousel.js
+++ carousel.js
@@ -1,17 +1,15 @@
 let animationUno = document.querySelector(".container-img:nth-child(1)");
 
 animationUno.addEventListener("animationiteration", () => {
-  console.log("Iteracion");
-  animationUno.style.right = "-100.2vw";
+  animationUno.style.right = "-100vw";
   animationUno.style.animationName = "carruselDos";
-    animationUno.style.animationDuration = "12s";
-  });
+  animationUno.style.animationDuration = "12s";
+});
 
 let animationDos = document.querySelector(".container-img:nth-child(2)");
 
 animationDos.addEventListener("animationiteration", () => {
-  console.log("IteracionDos");
   animationDos.style.right = "-50vw";
   animationDos.style.animationName = "carruselDos";
-    animationDos.style.animationDuration = "12s";
-  });
+  animationDos.style.animationDuration = "12s";
+});
